2ad02a3f1376d07846aea5766383c395b13e90cb
bahui80 <bahui80@hotmail.com>
2014-06-01 22:45:20 -0300

    parametros ya levantados bien. Falta nada mas ver como leer un directorio y agregarlo y estaria todo hecho

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ENCODE 0 // -d 
+#define DECODE 1 // -r
 void help();
-
 
 // PARAMETRO 0: nombre del programa.
 // PARAMETRO 1: -d, -r.
@@ -13,17 +14,19 @@ void help();
 
 int
 main(int argc, char * argv[]) {
-	int i;
+	int method, k, n;
+	char * filename;
+	FILE * image;
 
 	if(argc < 6 || argc > 10) {
 		help();
 		exit(EXIT_FAILURE);
 	}
 	
-	if(strcmp(argv[1],"-r") == 0) {
-		// LO GUARDO DE ALGUNA MANERA
-	} else if(strcmp(argv[1],"-d") == 0) {
-		// LO GUARDO DE ALGUNA MANERA
+	if(strcmp(argv[1],"-d") == 0) {
+		method = ENCODE; // SE VA A CODIFICAR LA IMAGEN EN OTRAS
+	} else if(strcmp(argv[1],"-r") == 0) {
+		method = DECODE; // SE VA A DECODIFICAR UNA IMAGEN A PARTIR DE OTRAS
 	} else {
 		help();
 		exit(EXIT_FAILURE);
@@ -34,16 +37,74 @@ main(int argc, char * argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	// argv[3] = "GUARDO EL NOMBRE DE ARCHIVO";
+	filename = argv[3];
+	if(strstr(filename, ".bmp") == NULL) {
+		printf("El archivo debe tener extensión .bmp\n");
+		exit(EXIT_FAILURE);
+	}
+	if(method == ENCODE) {
+		if((image = fopen(filename, "rb")) == NULL) {
+			printf("No se encontró el archivo\n");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		image = fopen(filename, "wb");
+	}
+	
 
 	if(strcmp(argv[4],"-k") != 0) {
 		help();
-		exit(EXIT_FAILURE);		
+		exit(EXIT_FAILURE);
 	}
 
-	// argv[5] = "GUARDO EL NUMERO QUE REPRESENTA A k";
-
-
+	k =  atoi(argv[5]);
+	if(k != 2 && k != 3 && k != 4) {
+		help();
+		exit(EXIT_FAILURE);
+	}
+	
+	if(argc > 6) {
+	      if(argc == 8) {
+			if(strcmp(argv[6], "-n") == 0) {
+				n = atoi(argv[7]);
+				if(n < 3 || n > 8 || method == DECODE) {
+					help();
+					exit(EXIT_FAILURE);
+				}
+			} else if(strcmp(argv[6], "-dir") == 0) {
+				// SE GUARDA EL DIRECTORIO DE DONDE SE LEEN O DONDE SE ESCRIBEN LAS IMAGENES CON EL SECRETO argv[7]
+			} else {
+				help();
+				exit(EXIT_FAILURE);
+			}
+	      } else if(argc == 10) {
+			if(strcmp(argv[6], "-n") != 0) {
+				help();
+				exit(EXIT_FAILURE);
+			} 
+			n = atoi(argv[7]);
+			if(n < 3 || n > 8 || method == DECODE) {
+				help();
+				exit(EXIT_FAILURE);
+			}
+			if(strcmp(argv[8], "-dir") != 0) {
+				help();
+				exit(EXIT_FAILURE);
+			}
+			// SE GUARDA EL DIRECTORIO DE DONDE SE LEEN O DONDE SE ESCRIBEN LAS IMAGENES CON EL SECRETO argv[9]
+	      } else {
+			help();
+			exit(EXIT_FAILURE);
+	      }
+	} else {
+		// SE ELIGEN LOS PARAMETROS DEFAULT QUE DICE LA CONSIGNA
+		// SERIAN: EL N EN CASO DE QUE SE HAYA USADO -D Y EL DIRECTORIO ACTUAL PARA LAS IMAGENES
+	}
+	
+	
+	// ACA SE CARGAN LAS IMAGENES DEL DIRECTORIO
+	
+	fclose(image);
 	return EXIT_SUCCESS;	
 }
 
@@ -53,7 +114,7 @@ help() {
 	printf("arg1 = -d: indica que se va a distribuir una imagen secreta en otras imágenes\n");
 	printf("%88s","-r: indica que se va a recuperar una imagen secreta a partir de otras imágenes\n\n");
 	printf("arg2 = -secret imagen: el nombre imagen corresponde al nombre de un archivo de extensión .bmp. En el caso de que se haya elegido la opción (-d) éste archivo debe existir ya que es la imagen a ocultar. Si se eligió la opción (-r) éste archivo será el archivo de salida, con la imagen secreta revelada al finalizar el programa\n\n");
-	printf("arg3 = -k número: el número corresponde a la cantidad mínima de sombras necesarias para recuperar el secreto en un esquema (k, n)\n\n");
-	printf("arg4 = -n número: el número corresponde a la cantidad total de sombras en las que se distribuirá el secreto en un esquema (k, n). Sólo puede usarse en el caso de que se haya elegido la opción (-d). Si no se usa, el programa elegirá como valor de n la cantidad total de imágenes del directorio\n\n");
+	printf("arg3 = -k número: el número corresponde a la cantidad mínima de sombras necesarias para recuperar el secreto en un esquema (k, n). Sólo se tendrá en cuenta un valor de k igual a 2, 3 ó 4\n\n");
+	printf("arg4 = -n número: el número corresponde a la cantidad total de sombras en las que se distribuirá el secreto en un esquema (k, n). Sólo puede usarse en el caso de que se haya elegido la opción (-d). Si no se usa, el programa elegirá como valor de n la cantidad total de imágenes del directorio. El valor de n será de, mínimo 3 y máximo 8\n\n");
 	printf("arg5 = -dir directorio: el directorio donde se encuentran las imágenes en las que se distribuirá el secreto (en el caso de que se haya elegido la opción (-d)), o donde están las imágenes que contienen oculto el secreto ( en el caso de que se haya elegido la opción (-r)). Si no se usa, el programa buscará las imágenes en el directorio actual\n\n");
 }
