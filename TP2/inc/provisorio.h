2c0c8fb1fa88e534c8a7b047a39f4b55d3f288a1
MartuPuri <martinpurita@gmail.com>
2014-06-19 19:30:49 -0300

    Add recover k = 2

--- TP2/inc/provisorio.h
+++ TP2/inc/provisorio.h
@@ -6,9 +6,12 @@
 #include <math.h>
 #include "lib.h"
 
+int * valuesFork3(int ** mat, int k, int * error);
 Image recoverSecretImage(Image * shadowImages, int amountOfBytes, int k, int * error);
 int detVertical3x3(int ** mat);
 int detHorizontal3x3(int ** mat);
 int calculate_inverse(int b);
+int  det2x2(int ** mat);
+int * valuesFork2(int ** mat, int k, int * error);
 
 #endif
--- TP2/src/provisorio.c
+++ TP2/src/provisorio.c
@@ -36,10 +36,6 @@ recoverSecretImage(Image * shadowImages, int amountOfBytes, int k, int * error) 
 		return NULL;
 	}
 	for (sizeIndex = 0; sizeIndex < amountOfBytes; sizeIndex += k) {
-		if (sizeIndex == 54) {
-			printf("");
-		}
-		// printf("sizeIndex: %d\n", sizeIndex);
 		int ** mat = calloc(sizeof(int *), k);
 		if (mat == NULL) {
 			free(originalImage);
@@ -57,28 +53,25 @@ recoverSecretImage(Image * shadowImages, int amountOfBytes, int k, int * error) 
 				return NULL;
 			}
 		}
-		// int mat[3][4];
 		for (shadowImagesIndex = 0; shadowImagesIndex < k; shadowImagesIndex++) {
 			BYTE * image = getImage(shadowImages[shadowImagesIndex]);
 			int auxB = 0;
-
-			// printf("shadowImagesIndex: %d\n", shadowImagesIndex);
-			// printf("sizeIndex %d\n", sizeIndex);
 			for (index = 0; index < k - 1; index++) {
-				if (sizeIndex == superValue) {
-					printf("Image%d: %d\n ", shadowImagesIndex, image[sizeIndex + index]);
+				// if (sizeIndex == 2) {
+				// 	printf("Image %d value = %X\n", shadowImagesIndex, image[sizeIndex + index]);
+				// }
+				if (k == 3) {
+					mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfAi) >> 3);
+				} else if (k == 2) {
+					// if (sizeIndex == 2) {
+					// 	printf("((((int) image[sizeIndex + index]) & amountOfAi) >> 4): %d\n", ((((int) image[sizeIndex + index]) & amountOfAi) >> 4));
+					// 	printf("and: %s\n", byte_to_binary((((int) image[sizeIndex + index]) & amountOfAi)));
+					// 	printf("amount: %s\n", byte_to_binary(amountOfAi));
+					// 	printf("binary: %s\n", byte_to_binary((int) image[sizeIndex + index]));
+					// }
+					mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfAi) >> 4);
 				}
-				// printf("image %s\n", byte_to_binary(image[index]));
-				// printf("(int) image[sizeIndex + index]: %d\n", (int) image[sizeIndex + index]);
-				if (sizeIndex == superValue && shadowImagesIndex == 0) {
-					printf("((int) image[sizeIndex + index]) & amountOfBi: %s\n", byte_to_binary(((int) image[sizeIndex + index]) & amountOfBi));
-				} 
-				mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfAi) >> 3);
-				// printf("In binary: %s\n SEcond %s\n", byte_to_binary((int) image[sizeIndex]), byte_to_binary(amountOfAi));
-				// printf("First ai: %d\n", mat[shadowImagesIndex][index]);
-				// printf("Value: %d\n", ((int) image[index]) & amountOfBi);
 				auxB += ((int) image[sizeIndex + index]) & amountOfBi;
-				// printf("auxB before <<: %d\n", auxB);
 				if (k == 2) {
 					auxB = auxB << 4;
 				} else if (k == 3) {
@@ -88,114 +81,160 @@ recoverSecretImage(Image * shadowImages, int amountOfBytes, int k, int * error) 
 						auxB = auxB << 3;
 					}
 				}
-				// printf("auxB after <<: %d\n", auxB);
-				// printf("auxB: %d\n", auxB);
 			}
-			mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfLastAi) >> 3);
-			if (sizeIndex == superValue && shadowImagesIndex == 0) {
-					printf("((int) image[sizeIndex + index]) & amountOfLastBi: %s\n", byte_to_binary(((int) image[sizeIndex + index]) & amountOfLastBi));
-				} 
-			// printf("image %s\n", byte_to_binary(image[index]));
-			// printf("%d In binary: %s SEcond %s\n", (int) image[index], byte_to_binary((int) image[index]), byte_to_binary(amountOfLastAi));
-			// if (sizeIndex == superValue) {
-			// 	printf("(int) image[sizeIndex + index]: %s\n", byte_to_binary((int) image[sizeIndex + index]));
-			// 	printf("(int) image[sizeIndex + index]: %s\n", byte_to_binary(amountOfLastBi));
-			// 	printf("((int) image[index]) & amountOfLastBi: %d\n", ((int) image[sizeIndex + index]) & amountOfLastBi);
+			// if (sizeIndex == 2) {
+			// 	printf("Image %d value = %X\n", shadowImagesIndex, image[sizeIndex + index]);
+			// 	printf("((((int) image[sizeIndex + index]) & amountOfLastAi) >> 5): %d\n", ((((int) image[sizeIndex + index]) & amountOfLastAi) >> 5));
+			// 	printf("and: %s\n", byte_to_binary((((int) image[sizeIndex + index]) & amountOfLastAi)));
+			// 	printf("amount: %s\n", byte_to_binary(amountOfLastAi));
+			// 	printf("binary: %s\n", byte_to_binary((int) image[sizeIndex + index]));
 			// }
+			if (k == 3) {
+				mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfLastAi) >> 3);
+			} else if (k == 2) {
+				mat[shadowImagesIndex][index] = ((((int) image[sizeIndex + index]) & amountOfLastAi) >> 5);
+			}
 			auxB += ((int) image[sizeIndex + index]) & amountOfLastBi;
-			if (sizeIndex == superValue) {
-					printf("Image%d: %d\n ", shadowImagesIndex, image[sizeIndex + index]);
-				}
-			// printf("auxB: %d\n", auxB);
 			mat[shadowImagesIndex][++index] = auxB % 251;
-		}
-		// printf("Determinantes");
-		if (sizeIndex == superValue) {
-			for (i = 0; i < k; i++) {
-				for (j = 0; j < k + 1; j++) {
-					printf("%d - ", mat[i][j]);
-				}
-				printf("\n");
-			}
-		}
 
-		int deltaS = detVertical3x3(mat);
-		int inverse = calculate_inverse(deltaS);
-		int ** mat2 = calloc(sizeof(int *), k);
-		if (mat2 == NULL) {
-			*error = CALLOC_ERROR;
-			return NULL;
 		}
-		for (i = 0; i < k; i++) {
-			mat2[i] = calloc(sizeof(int), k + 1);
-			if (mat2[i] == NULL) {
-				free(mat);
-				*error = CALLOC_ERROR;
-				return NULL;
-			}
-		}
-		for (i = 0; i < k; i++) {
-			for (j = 0; j < k + 1; j++) {
-				mat2[i][j] = mat[i][j];
-			}
-		} 
-		for(i = 0; i < k; i++) {
-			mat2[i][0] = mat2[i][3];
-		}
-		// if (sizeIndex== 54) {
+		// if (sizeIndex < 20) {
+		// 	printf("Matri<\n");
 		// 	for (i = 0; i < k; i++) {
 		// 		for (j = 0; j < k + 1; j++) {
-		// 			printf("%d - ", mat2[i][j]);
+		// 			printf("%d - ", mat[i][j]);
 		// 		}
 		// 		printf("\n");
 		// 	}
 		// }
-		int deltaX = detHorizontal3x3(mat2);
-		int values[3];
-		values[0] = (deltaX * inverse) % 251;
-		for(i = 0; i < k; i++) {
-			mat2[i][0] = mat[i][0];
+		int * values;
+		if (k == 3) {
+			values = valuesFork3(mat, k, error);
+		} else if (k == 2) {
+			values = valuesFork2(mat, k, error);
 		}
-		for(i = 0; i < k; i++) {
-			mat2[i][1] = mat2[i][3];
-		}
-		int deltaY = detVertical3x3(mat2);
-		values[1] = (deltaY * inverse) % 251;
-		for(i = 0; i < k; i++) {
-			mat2[i][1] = mat[i][1];
-		}
-		for(i = 0; i < k; i++) {
-			mat2[i][2] = mat2[i][3];
-		}
-		int deltaZ = detHorizontal3x3(mat2);
-		values[2] = (deltaZ * inverse) % 251;
+		// if (sizeIndex < 20) {
+		// 		printf("Values: x = %d y = %d deltaS = %d deltaX = %d  deltaY = %d\n", values[0], values[1], values[2], values[3], values[4]);
+		// }
 		for(i = 0; i < k; i++) {
 			// printf("i + sizeIndex: %d values = %d\n", i + sizeIndex, values[i]);
 			setImageInIndex(originalImage, (char) values[i], i + sizeIndex);
 		}
-		if (sizeIndex == superValue) {
-printf("indverse=%d\ndeltaX=%d\ndeltaY=%d\ndeltaZ=%d\ndeltaS=%d\nx=%d\ny=%d\nz=%d\n", INVERSE[deltaS], deltaX, deltaY, deltaZ, deltaS, values[0], values[1], values[2]);
-		}
 	}
 
-	return originalImage;
-	// // PSEUDOCODIGO
-	// for(l = 0; l < tamaño imagen; l += k) { // 
-	// 	// TODO: VALIDAR QUE EL BIT DE PARIDAD ESTE BIEN ANTES DE SEGUIR
-	// 	b1 = concat(amountOfBi,amountOfBi, amountOfLastBi); // SACO EL VALOR DE B
-	
-	// 	a1 = amountOfAi; //SACO a1
-	// 	a2 = amountOfAi; // SACO a2 
-	// 	a3 = amountOfLastAi; // SACO a3
-
-	// 	a11 * x + a12 * y + a13 * z = b1
-	// 	a21 * x + a22 * y + a23 * z = b2
-	// 	a31 * x + a32 * y + a33 * z = b3
-	
-	// 	solve([a] , [b], k);
-	// }	
+	return originalImage;	
 }
 
+int *
+valuesFork2(int ** mat, int k, int * error) {
+	int i, j;
+	int * values = calloc(sizeof(int), k);
+	if (values == NULL) {
+		*error = CALLOC_ERROR;
+		return NULL;
+	}
+	int deltaS = det2x2(mat);
+	int inverse = calculate_inverse(deltaS);
+	int ** mat2 = calloc(sizeof(int *), k);
+	if (mat2 == NULL) {
+		*error = CALLOC_ERROR;
+		return NULL;
+	}
+	for (i = 0; i < k; i++) {
+		mat2[i] = calloc(sizeof(int), k + 1);
+		if (mat2[i] == NULL) {
+			free(mat);
+			*error = CALLOC_ERROR;
+			return NULL;
+		}
+	}
+	for (i = 0; i < k; i++) {
+		for (j = 0; j < k + 1; j++) {
+			mat2[i][j] = mat[i][j];
+		}
+	} 
+	for(i = 0; i < k; i++) {
+		mat2[i][0] = mat2[i][2];
+	}
+	int deltaX = det2x2(mat2);
+	values[0] = (deltaX * inverse) % 251;
+	for(i = 0; i < k; i++) {
+		mat2[i][0] = mat[i][0];
+	}
+	for(i = 0; i < k; i++) {
+		mat2[i][1] = mat2[i][2];
+	}
+	int deltaY = det2x2(mat2);
+	values[1] = (deltaY * inverse) % 251;
+	values[2] = deltaS;
+	values[3] = deltaX;
+	values[4] = deltaY;
+	return values;
+}
+
+int 
+det2x2(int ** mat) {
+	int left = (mat[0][0] * mat[1][1]) % 251;
+	int right = (mat[0][1] * mat[1][0]) % 251;
+	int result = (left - right);
+	while (result < 0) {
+		result += 251;
+	}
+	result = result % 251;
+	return result;
+}
+
+int *
+valuesFork3(int ** mat, int k, int * error) {
+	int i, j;
+	int * values = calloc(sizeof(int), k);
+	if (values == NULL) {
+		*error = CALLOC_ERROR;
+		return NULL;
+	}
+	int deltaS = detVertical3x3(mat);
+	int inverse = calculate_inverse(deltaS);
+	int ** mat2 = calloc(sizeof(int *), k);
+	if (mat2 == NULL) {
+		*error = CALLOC_ERROR;
+		return NULL;
+	}
+	for (i = 0; i < k; i++) {
+		mat2[i] = calloc(sizeof(int), k + 1);
+		if (mat2[i] == NULL) {
+			free(mat);
+			*error = CALLOC_ERROR;
+			return NULL;
+		}
+	}
+	for (i = 0; i < k; i++) {
+		for (j = 0; j < k + 1; j++) {
+			mat2[i][j] = mat[i][j];
+		}
+	} 
+	for(i = 0; i < k; i++) {
+		mat2[i][0] = mat2[i][3];
+	}
+	int deltaX = detHorizontal3x3(mat2);
+	values[0] = (deltaX * inverse) % 251;
+	for(i = 0; i < k; i++) {
+		mat2[i][0] = mat[i][0];
+	}
+	for(i = 0; i < k; i++) {
+		mat2[i][1] = mat2[i][3];
+	}
+	int deltaY = detVertical3x3(mat2);
+	values[1] = (deltaY * inverse) % 251;
+	for(i = 0; i < k; i++) {
+		mat2[i][1] = mat[i][1];
+	}
+	for(i = 0; i < k; i++) {
+		mat2[i][2] = mat2[i][3];
+	}
+	int deltaZ = detHorizontal3x3(mat2);
+	values[2] = (deltaZ * inverse) % 251;
+	return values;
+}
 
 // Vertical (3x3):
 int
